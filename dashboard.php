75ca02525f845329f321ba53d75c53634535f9e6
Alyx1234Hi5 <151732982+Alyx1234Hi5@users.noreply.github.com>
2024-11-23 14:53:06 +0800

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -348,7 +348,7 @@ if(!isset($_SESSION['user_role'])) {
                 <div class="modal-dialog modal-lg">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="rushModalLabel"> Requests</h5>
+                            <h5 class="modal-title" id="rushModalLabel"> Rush Requests</h5>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">

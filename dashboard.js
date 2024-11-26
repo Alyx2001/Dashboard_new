52be959e3e3d7698b2f1e4df0778eb3c149c2272
Alyx1234Hi5 <151732982+Alyx1234Hi5@users.noreply.github.com>
2024-11-27 00:07:40 +0800

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,83 +1,124 @@
 
 // ********* Notification MOdal ********* //
 
-// Update Pickup Remarks
-$('.pickup-checkbox').on('change', function() {
-    var $checkbox = $(this);  
-    var customerId = $checkbox.data('id');
-    var remark = $checkbox.data('remark');
+    $('.pickup-checkbox').on('change', function() {
+        var $checkbox = $(this);  
+        var customerId = $checkbox.data('id');
+        var remark = $checkbox.data('remark');
 
-    $.ajax({
-        url: 'update_remark.php',
-        type: 'POST',
-        data: {
-            customer_id: customerId,
-            remark: remark,
-            type: 'pickup'  
-        },
-        success: function(response) {
-            console.log('Response from update_remark.php:', response); 
+            Swal.fire({
+                title: 'Are you sure?',
+                text: 'Do you want to mark this as ' + remark + '?',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'Yes',
+                cancelButtonText: 'No',
+                reverseButtons: true,
+                customClass: {
+                    confirmButton: 'swal-confirm-btn',
+                    cancelButton: 'swal-cancel-btn'
+                }
+        }).then((result) => {
+            if (result.isConfirmed) {
+            
+                $.ajax({
+                    url: 'update_remark.php',
+                    type: 'POST',
+                    data: {
+                        customer_id: customerId,
+                        remark: remark,
+                        type: 'pickup'  
+                    },
+                    success: function(response) {
+                        console.log('Response from update_remark.php:', response); 
 
-            if (remark === 'Claimed') {
-                $checkbox.closest('.notification-item').remove(); 
-                updatePickupRemarks();  
+                
+                        if (remark === 'Claimed' || remark === 'Undelivered' || remark === 'Unclaimed' || remark === 'Pending') {
+                            $checkbox.closest('.notification-item').remove(); 
+                            updatePickupRemarks();  
+                        }
+                    },
+                    error: function(xhr, status, error) {
+                        console.error('AJAX error:', error); 
+                    }
+                });
+            } else {
+            
+                $checkbox.prop('checked', !$checkbox.prop('checked'));
             }
-        },
-        error: function(xhr, status, error) {
-            console.error('AJAX error:', error); 
-        }
+        });
     });
-});
 
-// Update Delivery Remarks
-$('.delivery-checkbox').on('change', function() {
-    var $checkbox = $(this);  
-    var customerId = $checkbox.data('id');
-    var remark = $checkbox.data('remark');
 
-    $.ajax({
-        url: 'update_remark.php',
-        type: 'POST',
-        data: {
-            customer_id: customerId,
-            remark: remark,
-            type: 'delivery'
-        },
-        success: function(response) {
-            console.log('Response from update_remark.php:', response);
+    $('.delivery-checkbox').on('change', function() {
+        var $checkbox = $(this);  
+        var customerId = $checkbox.data('id');
+        var remark = $checkbox.data('remark');
 
-            if (remark === 'Delivered') {
-                $checkbox.closest('.notification-item').remove(); 
-                updateDeliveryRemarks();  
+    
+        Swal.fire({
+            title: 'Are you sure?',
+            text: 'Do you want to mark this as ' + remark + '?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Yes',
+            cancelButtonText: 'No',
+            reverseButtons: true,
+            customClass: {
+                confirmButton: 'swal-confirm-btn',
+                cancelButton: 'swal-cancel-btn'
             }
-        },
-        error: function(xhr, status, error) {
-            console.error('AJAX error:', error);
-        }
+        }).then((result) => {
+            if (result.isConfirmed) {
+        
+                $.ajax({
+                    url: 'update_remark.php',
+                    type: 'POST',
+                    data: {
+                        customer_id: customerId,
+                        remark: remark,
+                        type: 'delivery'
+                    },
+                    success: function(response) {
+                        console.log('Response from update_remark.php:', response);
+
+                    
+                        if (remark === 'Delivered' || remark === 'Undelivered' || remark === 'Unclaimed' || remark === 'Pending') {
+                            $checkbox.closest('.notification-item').remove(); 
+                            updateDeliveryRemarks();  
+                        }
+                    },
+                    error: function(xhr, status, error) {
+                        console.error('AJAX error:', error);
+                    }
+                });
+            } else {
+            
+                $checkbox.prop('checked', !$checkbox.prop('checked'));
+            }
+        });
     });
-});
 
-// Function to update Pickup remarks in the UI
-function updatePickupRemarks() {
-    console.log('Updating Pickup remarks...');
-}
 
-// Function to update Delivery remarks in the UI
-function updateDeliveryRemarks() {
-    console.log('Updating Delivery remarks...');
-}
+    function updatePickupRemarks() {
+        console.log('Updating Pickup remarks...');
+    }
 
-// Reload the modal when it's closed
-$(document).ready(function() {
-    $('#pickupModal').on('hidden.bs.modal', function () {
-        location.reload();  
+    function updateDeliveryRemarks() {
+        console.log('Updating Delivery remarks...');
+    }
+
+    // Reload the modal when it's closed
+    $(document).ready(function() {
+        $('#pickupModal').on('hidden.bs.modal', function () {
+            location.reload();  
+        });
+
+        $('#deliveryModal').on('hidden.bs.modal', function () {
+            location.reload();  
+        });
+
+        $('#rushModal').on('hidden.bs.modal', function () {
+            location.reload();  
+        });
     });
-
-    $('#deliveryModal').on('hidden.bs.modal', function () {
-        location.reload();  
-    });
-
-    $('#rushModal').on('hidden.bs.modal', function () {
-        location.reload();  
-    });
-});
